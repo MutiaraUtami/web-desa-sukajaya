d3639f94a1281b1295bf5e767b56095113e420b0
RizBarr <2310631170115@student.unsika.ac.id>
2026-07-09 11:06:32 +0700

    chore: setup awal laravel, autentikasi, dan tabel profil desa

---
+++ app/Models/ProfilDesa.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProfilDesa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'profil_desa';
+
+    protected $fillable = [
+        'nama_desa',
+        'logo',
+        'sejarah',
+        'visi',
+        'misi',
+        'geografis'
+    ];
+}
