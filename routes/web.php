2edb6c4430ab5f22dae1ee0b22c768064bff4bc3
RizBarr <2310631170115@student.unsika.ac.id>
2026-07-09 12:17:52 +0700

    feat: membuat controller CRUD profil desa dan view dummy

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProfilDesaController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
-});
+    Route::get('/profil-desa', [ProfilDesaController::class, 'index'])->name('profil-desa.index');
+    Route::post('/profil-desa/update', [ProfilDesaController::class, 'update'])->name('profil-desa.update');
+    });
 
 require __DIR__.'/settings.php';
